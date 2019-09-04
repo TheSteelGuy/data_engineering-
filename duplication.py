5b52ffca9155f829f3d8e5c2a623aac6c7c4478c
TheSteelGuy <collotuos0@gmail.com>
2019-09-04 18:54:25 +0300

    refactor code

---
+++ duplication.py
@@ -0,0 +1,45 @@
+import re
+def code_duplication_check(lines):
+    """[check the a file for code duplication]
+    
+    Arguments:
+        file_ {[str]} -- [content of the current file]
+    """
+    # get indexes of all '""""' 
+    lines = [
+        line.strip() for line in lines if len(line.strip())>0 and not re.match(r'^#.+', line.strip())]
+    docstrings_remove_final = []
+    docstrings_remove = []
+    # get indexes of all '"""'
+    idx = [i for i,v in enumerate(lines) if v in ([ '"""'] or "'''")]
+    for i in range(len(idx)): 
+        if i%2==0 and len(idx) > 2:
+            idx[i] = idx[i]+1
+            docstrings_remove.extend([lines[idx[index]:idx[index+1]] for index in range(0, len(idx)-1,2)])
+        elif i%2==0 and len(idx) == 2:
+            docstrings_remove.extend([lines[idx[0]:idx[1]] for index in range(0, len(idx)-1,2)])
+    # list of docstrings to be removed
+    #docstrings_remove = [lines[idx[index]:idx[index+1]][0] for index in range(0, len(idx)-1,2)]
+    for item in docstrings_remove:
+        for docs in item:
+            docstrings_remove_final.append(docs) 
+    lines = [
+        item.strip() for item in lines if item not in docstrings_remove and not\
+                (item.startswith('"""') or item.startswith("'''"))]
+    file_str = ''
+    count = 0
+    line_length = 0
+    while line_length <= len(lines)-1:
+        if count < 4:    
+            file_str += lines[line_length]
+            count += 1
+        elif count == 4:
+            file_str += '<~>'
+            count = 0
+
+        line_length += 1
+
+    file_list = file_str.split('<~>')
+    file_list[:] = [s for s in file_list if len(s.strip())>0]
+    duplicates = len(file_list) - len(set(file_list))
+    return duplicates
---
+++ main_file.py
@@ -0,0 +1,75 @@
+import re
+from github_retrieve import (
+    traverse_repos,
+    code_duplication_check,
+    find_repo_imports,
+    count_lines_of_code,
+    find_for_loops,
+    avarage_parameters,
+    avarage_variables_per_line,
+    find_docstrings_and_comments,
+    find_external_packages,
+    nesting_depth
+)
+def single_line_in_file():
+    """ 
+    function helps looping once on each line to perform all operations 
+    """
+    # contains the result of the total repositories
+    result = [] 
+    count_all_lines = 0 # mainatains the line that has been counted
+    for_loops_list = []
+    func_parameters = [] # function parameter checks list
+    no_of_variables = set() # a set containing variables
+    docs_comments = []  # list of 
+    single_line_comments = []
+    code_duplication = 0
+    repo_imports_set = set() # imports for the entire repo
+    current_repo = ''
+    for item in traverse_repos():
+        current_repo = item['repo_url']
+        for path in item['files']:
+            with open(path, 'r') as file_:
+                lines = file_.readlines()
+                # call code duplication
+                code_duplication  += code_duplication_check(lines)
+                for line in lines:
+                    if re.match(r'^#.+', line.strip()):
+                        single_line_comments.append(line.strip())
+                        # this makes it possible to campare later
+                    # call find_repo_imports
+                    line_import = find_repo_imports(line)
+        
+                    repo_imports_set.add(line_import)
+                    # call countlines of code function
+                    count_all_lines += count_lines_of_code(line.strip())
+                    # call find_for_loops
+                    for_loops = find_for_loops(line)
+                    if for_loops:
+                        for_loops_list.append(for_loops)
+                    function = avarage_parameters(line)
+                    if function:
+                        func_parameters.append(avarage_parameters(line))
+                    no_of_variables.add(avarage_variables_per_line(line))
+        
+            with open(path, 'r') as content_file:
+                content = content_file.read()
+                docs_comments.extend(find_docstrings_and_comments(content, single_line_comments))
+        external_packages = find_external_packages(repo_imports_set)
+        repo_lines_of_codes = count_all_lines - len(docs_comments)
+        avarage_variables_repo = (len(no_of_variables)-1) / repo_lines_of_codes
+        nesting = nesting_depth(for_loops_list) / len(for_loops_list)
+        avarage_params = sum(func_parameters) / len(func_parameters)
+        repo_result = {
+                    'repository_url': current_repo, 
+                    'number of lines': repo_lines_of_codes, 
+                    'libraries': external_packages,
+                    'nesting factor': nesting,
+                    'code duplication': code_duplication,
+                    'average parameters': avarage_params,
+                    'average variables': avarage_variables_repo
+                
+            }
+        result.append(repo_result)
+    return result
+print(single_line_in_file()) # make it return stuffs
